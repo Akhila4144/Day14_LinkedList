dd4d14f863819beaa55fa8e9254b3d7853bb3e25
Akhila4144 <orugantiakhila34@gmail.com>
2021-12-07 14:50:43 +0530

    UC6 Done

---
+++ LinkedListUC6/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LinkedListUC6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<int> linkedList = new LinkedList<int>();
+            linkedList.AddFirst(56);
+            linkedList.AddLast(30);
+            linkedList.AddLast(70);
+            foreach (int value in linkedList)
+            {
+                Console.WriteLine(value);
+            }
+            linkedList.RemoveLast();
+            Console.WriteLine("After removing... The LinkedList Will be:");
+            foreach (int value in linkedList)
+            {
+                Console.WriteLine(value);
+            }
+
+        }
+    }
+}
