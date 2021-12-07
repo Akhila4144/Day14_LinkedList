b92bc1c3764e34f14f6b9498a1da28723bb24cd0
Akhila4144 <orugantiakhila34@gmail.com>
2021-12-07 13:38:19 +0530

    UC2 Done By Adding Elements To LinkedList

---
+++ LinkedListUC2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LinkedListUC2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<int> linkedList = new LinkedList<int>();
+            linkedList.AddFirst(70);
+            linkedList.AddLast(30);
+            linkedList.AddLast(56);
+            foreach(int item in linkedList)
+            {
+                Console.WriteLine(item);
+            }
+            
+        }
+    }
+}
