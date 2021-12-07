46aee958701c02ba1f5965f7d48c0d6b7d99d25c
Akhila4144 <orugantiakhila34@gmail.com>
2021-12-07 14:47:51 +0530

    UC5 Done

---
+++ LinkedListUC5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LinkedListUC5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<int> linkedList = new LinkedList<int>();
+            linkedList.AddFirst(56);
+            linkedList.AddLast(30);
+            linkedList.AddLast(70);
+            foreach(int value in linkedList)
+            {
+                Console.WriteLine(value);
+            }
+            linkedList.RemoveFirst();
+            Console.WriteLine("After removing the LinkedList Will be:");
+            foreach(int value in linkedList)
+            {
+                  Console.WriteLine(value);
+            }
+
+        }
+    }
+}
